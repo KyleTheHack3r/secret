14ab40ffcebc4804c6eb9c170e4f1b8ff1477cc2
KyleTheHack3r <thepmteam@hotmail.com>
2015-05-06 22:12:53 +0100

    Create apple_golden.php

---
+++ food/apple_golden.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Apple_Golden extends Food{
+	$apple_golden = setFood(505,"apple_golden", 0,20,"Golden Apple");
+        $apple_goldenCraft = setCraft(505,1,0,[260,1,0,266,8,0]);
+	}
+
+}
+?>
